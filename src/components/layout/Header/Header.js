674acc998eae3c443801350e09401060114a4076
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-09 16:13:09 +0200

    updated header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -2,34 +2,73 @@ import { NavLink } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faBagShopping,
-    faUser,
-    faGem,
-    faHeart,
-  } from "@fortawesome/free-solid-svg-icons";
+  faBagShopping,
+  faUser,
+  faGem,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
+import "./Header.scss";
 import { LINK_TITLES } from "./constants/link_titles";
 
 export const Header = () => {
   return (
-    <header className={styles["main-header"]}>
-      <ul role="list">
-        <li>
-          <NavLink
-            end
-            className={({ isActive }) =>
-              isActive ? styles["selected"] : styles["not-selected"]
-            }
-            to={to}
-          >
-            <FontAwesomeIcon icon={faGem} className={styles["icon"]} />
-            Collection
-          </NavLink>
-        </li>
-        <li>Wishlist</li>
-        <li>My Bag</li>
-        <li>Account</li>
-      </ul>
+    <header>
+      <nav>
+        <ul role="list">
+          <li>
+            <NavLink
+              end
+              className={({ isActive }) =>
+                isActive ? "selected" : "not-selected"
+              }
+              to={"/collection"}
+            >
+              <FontAwesomeIcon icon={faGem} />
+              Collection
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink
+              end
+              className={({ isActive }) =>
+                isActive ? styles["selected"] : styles["not-selected"]
+              }
+              to={"/wishlist"}
+            >
+              <FontAwesomeIcon icon={faHeart} className={styles["icon"]} />
+              Wishlist
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              end
+              className={({ isActive }) =>
+                isActive ? styles["selected"] : styles["not-selected"]
+              }
+              to={"/shopping-bag"}
+            >
+              <FontAwesomeIcon
+                icon={faBagShopping}
+                className={styles["icon"]}
+              />
+              My Bag
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              end
+              className={({ isActive }) =>
+                isActive ? styles["selected"] : styles["not-selected"]
+              }
+              to={"/account"}
+            >
+              <FontAwesomeIcon icon={faUser} className={styles["icon"]} />
+              Account
+            </NavLink>
+          </li> */}
+        </ul>
+      </nav>
     </header>
   );
 };
