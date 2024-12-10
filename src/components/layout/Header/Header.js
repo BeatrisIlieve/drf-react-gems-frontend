9aa2f8d7e7a223ec7c38548daea5020dc684bb44
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-10 17:48:36 +0200

    updated collection

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { LogoImage } from "./LogoImage/LogoImage";
 import { CollectionLink } from "./CollectionLink/CollectionLink";
@@ -9,95 +9,90 @@ import { QuantityIndicatedLink } from "./reusable/QuantityIndicatedLink/Quantity
 
 import { faHeart, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
-import styles from "./Header.module.scss";
-
-// export const Header = () => {
-//   const wishlistCount = 2;
-//   const bagCount = 3;
-
-//   return (
-//     <header>
-//       <LogoImage />
-//       <nav>
-//         <ul role="list">
-//           <li>
-//             <CollectionLink />
-//           </li>
-//           <li>
-//             <SearchButton />
-//           </li>
-//           <li>
-//             <AccountLink />
-//           </li>
-//           <li>
-//             <QuantityIndicatedLink
-//               url={"/wishlist"}
-//               icon={faHeart}
-//               quantity={wishlistCount}
-//             />
-//           </li>
-//           <li>
-//             <QuantityIndicatedLink
-//               url={"/shopping-bag"}
-//               icon={faShoppingBag}
-//               quantity={bagCount}
-//             />
-//           </li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
+import styles from "./Header.module.css";
 
 export const Header = () => {
   const wishlistCount = 2;
   const bagCount = 3;
 
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // Only hide when scrolled past 6rem (96px) and scrolling down
+      if (currentScrollY > 96 && currentScrollY > lastScrollY) {
+        setIsHidden(true);
+      } else if (currentScrollY < lastScrollY) {
+        // Always show when scrolling up
+        setIsHidden(false);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <header>
-      <LogoImage />
-      <nav>
-        <ul role="list">
-          <li>
-            <CollectionLink label={"Earring"} url={"/earring"}/>
-          </li>
-          <li>
-            <CollectionLink label={"Necklace"} url={"/necklace"}/>
-          </li>
-          <li>
-            <CollectionLink label={"Pendant"} url={"/pendant"}/>
-          </li>
-          <li>
-            <CollectionLink label={"Charm"} url={"/charm"}/>
-          </li>
-          <li>
-            <CollectionLink label={"Bracelet"} url={"/bracelet"}/>
-          </li>
-          <li>
-            <CollectionLink label={"Ring"} url={"/ring"}/>
-          </li>
-          <li>
-            <SearchButton />
-          </li>
-          <li>
-            <AccountLink />
-          </li>
-          <li>
-            <QuantityIndicatedLink
-              url={"/wishlist"}
-              icon={faHeart}
-              quantity={wishlistCount}
-            />
-          </li>
-          <li>
-            <QuantityIndicatedLink
-              url={"/shopping-bag"}
-              icon={faShoppingBag}
-              quantity={bagCount}
-            />
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div
+      className={`${styles["header-container"]} ${
+        isHidden ? styles["hidden"] : styles["visible"]
+      }`}
+    >
+      <header className={styles["header"]}>
+        <nav className={styles["nav"]}>
+          <ul className={styles["list"]} role="list">
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Earring"} url={"/earring"} />
+            </li>
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Necklace"} url={"/necklace"} />
+            </li>
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Pendant"} url={"/pendant"} />
+            </li>
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Charm"} url={"/charm"} />
+            </li>
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Bracelet"} url={"/bracelet"} />
+            </li>
+            <li className={styles["list-item"]}>
+              <CollectionLink label={"Ring"} url={"/ring"} />
+            </li>
+            <li >
+              <LogoImage isHidden={isHidden} />
+            </li>
+            <li className={styles["list-item"]}>
+              <SearchButton />
+            </li>
+            <li className={styles["list-item"]}>
+              <AccountLink />
+            </li>
+            <li className={styles["list-item"]}>
+              <QuantityIndicatedLink
+                url={"/wishlist"}
+                icon={faHeart}
+                quantity={wishlistCount}
+              />
+            </li>
+            <li className={styles["list-item"]}>
+              <QuantityIndicatedLink
+                url={"/shopping-bag"}
+                icon={faShoppingBag}
+                quantity={bagCount}
+              />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
--- src/components/layout/Header/LogoImage/LogoImage.js
+++ src/components/layout/Header/LogoImage/LogoImage.js
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 
-import styles from "./LogoImage.module.scss";
+import styles from "./LogoImage.module.css";
 
-export const LogoImage = () => {
+export const LogoImage = ({ isHidden }) => {
   return (
     <Link to={"/"}>
-      <div className={styles.container}>
+      <div
+        className={`${styles["container"]} ${
+          isHidden ? styles["hidden"] : ""
+        }`.trim()}
+      >
         <img
+          className={styles["logo-img"]}
           src={
             "https://res.cloudinary.com/deztgvefu/image/upload/v1726147711/forget-me-not-collection/miniImages/logo2_zfmuo1.png"
           }
           alt="logo-image"
+        />
+        <img
+          className={styles["text-img"]}
+          src="https://res.cloudinary.com/deztgvefu/image/upload/v1733837454/forget-me-not-collection/miniImages/Screenshot_2024-12-10_at_15.29.46_vs8dyj.png"
+          alt="logo-text"
         />
       </div>
     </Link>
