45d16406533af23248a580da98e10d7d929905bc
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-10 05:12:04 +0200

    updated collection

--- src/components/pages/Collection/Collection.js
+++ src/components/pages/Collection/Collection.js
@@ -10,6 +10,31 @@ export const Collection = () => {
             alt="forget-me-not-collection"
           />
         </div>
+        <div>
+          <p>
+            The enchanting and delicate beauty of a Forget-Me-Not flower in
+            bloom is captured in a series of fine jewelry designs that celebrate
+            the endless beauty of nature’s greatest gifts – rare gemstones and
+            flowers in bloom.
+          </p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <img
+            src="https://res.cloudinary.com/deztgvefu/image/upload/v1733797275/forget-me-not-collection/miniImages/fifty_pdp_forget-me-not_b_rqlq6j.avif"
+            alt="forget-me-not-collection"
+          />
+        </div>
+        <div>Products</div>
+      </article>
+      <article>
+        <div>
+          <img
+            src="https://res.cloudinary.com/deztgvefu/image/upload/v1733797275/forget-me-not-collection/miniImages/fifty_pdp_forget-me-not_a_fcpwwq.avif"
+            alt="forget-me-not-collection"
+          />
+        </div>
         <div>Products</div>
       </article>
     </section>
