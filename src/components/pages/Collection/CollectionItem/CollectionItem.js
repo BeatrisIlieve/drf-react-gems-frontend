5ac7860ca636c3ba3b526b7a6d4cd3bb3863c90b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-11 14:57:21 +0200

    updated collection

--- src/components/pages/Collection/CollectionItem/CollectionItem.js
+++ src/components/pages/Collection/CollectionItem/CollectionItem.js
@@ -6,25 +6,33 @@ export const CollectionItem = ({ product }) => {
       <button className={styles["collection-item__button"]}>
         Discover {product.category}
       </button>
-      <div className={styles["collection-item__stock-info"]}>
-        <span className={styles["collection-item__quantity"]}>
-          {product.quantity}
-        </span>
-        <span className={styles["collection-item__price"]}>
-          {product.price}
-        </span>
-      </div>
-      <div className={styles["collection-item__thumbnail"]}>
-        <img
-          className={styles["collection-item__image"]}
-          src={product.image}
-          alt={`${product.category}-image`}
-        />
-      </div>
-      <div className={styles["collection-item__available-colors"]}>
-        <span>images</span>
-        <span>images</span>
-        <span>images</span>
+      <div className={styles["collection-item__product"]}>
+        <div className={styles["collection-item__stock-info"]}>
+          <span className={styles["collection-item__quantity"]}>
+            {product.quantity}
+          </span>
+          <span className={styles["collection-item__price"]}>
+            {product.price}
+          </span>
+        </div>
+        <div className={styles["collection-item__interaction-icons"]}>
+          <span className={styles["collection-item__switch-image"]}>
+            switch
+          </span>
+          <span className={styles["collection-item__like-product"]}>like</span>
+        </div>
+        <div className={styles["collection-item__thumbnail"]}>
+          <img
+            className={styles["collection-item__image"]}
+            src={product.image}
+            alt={`${product.category}-image`}
+          />
+        </div>
+        <div className={styles["collection-item__available-colors"]}>
+          <span>images</span>
+          <span>images</span>
+          <span>images</span>
+        </div>
       </div>
     </div>
   );
