22c000dde1489ff5fcd365de2d18a49048109f20
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-12 13:22:30 +0200

    updated collection

--- src/components/pages/Collection/CollectionItem/Overview/ProductCard/ProductCard.js
+++ src/components/pages/Collection/CollectionItem/Overview/ProductCard/ProductCard.js
@@ -23,33 +23,33 @@ export const ProductCard = ({ data }) => {
   }, []);
 
   return (
-    <div className={styles["product"]}>
+    <div className={styles["product_card"]}>
       {product.length > 0 && (
-        <div className={styles["product"]}>
-          <button className={styles["product__button"]}>
+        <div className={styles["product_card"]}>
+          <button className={styles["product_card__button"]}>
             Discover {product[0].full_category_title}
           </button>
-          <div className={styles["product__product"]}>
-            <div className={styles["product__stock-info"]}>
-              <span className={styles["product__quantity"]}>
+          <div className={styles["product_card__product"]}>
+            <div className={styles["product_card__stock-info"]}>
+              <span className={styles["product_card__quantity"]}>
                 {product[0].min_price} - {product[0].max_price}
               </span>
-              <span className={styles["product__price"]}>
+              <span className={styles["product_card__price"]}>
                 {product[0].is_sold_out}
               </span>
             </div>
-            <div className={styles["product__interaction-icons"]}>
-              <span className={styles["product__switch-image"]}>switch</span>
-              <span className={styles["product__like-product"]}>like</span>
+            <div className={styles["product_card__interaction-icons"]}>
+              <span className={styles["product_card__switch-image"]}>switch</span>
+              <span className={styles["product_card__like-product"]}>like</span>
             </div>
-            <div className={styles["product__thumbnail"]}>
+            <div className={styles["product_card__thumbnail"]}>
               <img
-                className={styles["product__image"]}
+                className={styles["product_card__image"]}
                 src={product[0].product__first_image_url}
                 alt={`${product[0].full_category_title}-image`}
               />
             </div>
-            <div className={styles["product__available-colors"]}>
+            <div className={styles["product_card__available-colors"]}>
               <span>images</span>
               <span>images</span>
               <span>images</span>
