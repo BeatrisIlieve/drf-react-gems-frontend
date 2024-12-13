09cc3d5d6a34f90b5b4ee08081d47f3dfa5377d2
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-13 15:37:58 +0200

    updated header

--- src/components/layout/Header/SearchButton/SearchButton.js
+++ src/components/layout/Header/SearchButton/SearchButton.js
@@ -1,4 +1,4 @@
-import { Icon } from "../../../reusable/Icon/Icon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./SearchButton.module.scss";
@@ -6,8 +6,11 @@ import styles from "./SearchButton.module.scss";
 export const SearchButton = () => {
   return (
     <button className={styles["search-button"]}>
-      <Icon icon={faMagnifyingGlass} />
-      <span className={styles["label"]}>Search</span>
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        className={styles["search-button__icon"]}
+      />
+      <span className={styles["search-button__label"]}>Search</span>
     </button>
   );
 };
