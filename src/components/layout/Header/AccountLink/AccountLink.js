bbe502b519348b896e030b1594044af133cb9495
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-16 14:30:38 +0200

    updated auth

--- src/components/layout/Header/AccountLink/AccountLink.js
+++ src/components/layout/Header/AccountLink/AccountLink.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Icon } from "../../../reusable/Icon/Icon";
 
 import styles from "./AccountLink.module.scss";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Auth } from "../../../modals/Auth/Auth";
 
-export const AccountLink = () => {
+export const AccountLink = ({accountButtonClickHandler}) => {
+
+
   return (
-    <Link to={"/register"} className={styles["link"]}>
-      <div>
-        <span></span>
+    <>
+      <div className={styles["container"]} onClick={accountButtonClickHandler}>
+        <div>
+          <span></span>
+        </div>
+        <Icon icon={faUser} />
       </div>
-      <Icon icon={faUser} />
-    </Link>
+    </>
   );
 };
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -7,7 +7,7 @@ import { AccountLink } from "./AccountLink/AccountLink";
 import { ProductSetDiscount } from "./ProductSetDiscount/ProductSetDiscount";
 
 import { QuantityIndicatedLink } from "./reusable/QuantityIndicatedLink/QuantityIndicatedLink";
-
+import { Auth } from "../../modals/Auth/Auth";
 import { faHeart, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.scss";
 
@@ -38,33 +38,49 @@ export const Header = () => {
     };
   }, [lastScrollY]);
 
+  const isAuthenticated = false;
+  const [displayAuthModal, setDisplayAuthModal] = useState(false);
+
+  const accountButtonClickHandler = () => {
+    if (!isAuthenticated) {
+      setDisplayAuthModal(true);
+    } else {
+      setDisplayAuthModal(false);
+    }
+  };
+
   return (
-    <div
-      className={`${styles["container"]} ${
-        isHidden ? styles["container_hidden"] : styles["container_visible"]
-      }`}
-    >
-      <ProductSetDiscount/>
-      <header className={styles["container__header"]}>
-        <nav className={styles["container__nav"]}>
-          <CollectionLink />
-          <LogoImage />
-          <div className={styles["container__buttons"]}>
-            <SearchButton />
-            <AccountLink />
-            <QuantityIndicatedLink
-              url={"/wishlist"}
-              icon={faHeart}
-              quantity={wishlistCount}
-            />
-            <QuantityIndicatedLink
-              url={"/shopping-bag"}
-              icon={faShoppingBag}
-              quantity={bagCount}
-            />
-          </div>
-        </nav>
-      </header>
-    </div>
+    <>
+      {displayAuthModal && <Auth />}
+      <div
+        className={`${styles["container"]} ${
+          isHidden ? styles["container_hidden"] : styles["container_visible"]
+        }`}
+      >
+        <ProductSetDiscount />
+        <header className={styles["container__header"]}>
+          <nav className={styles["container__nav"]}>
+            <CollectionLink />
+            <LogoImage />
+            <div className={styles["container__buttons"]}>
+              <SearchButton />
+              <AccountLink
+                accountButtonClickHandler={accountButtonClickHandler}
+              />
+              <QuantityIndicatedLink
+                url={"/wishlist"}
+                icon={faHeart}
+                quantity={wishlistCount}
+              />
+              <QuantityIndicatedLink
+                url={"/shopping-bag"}
+                icon={faShoppingBag}
+                quantity={bagCount}
+              />
+            </div>
+          </nav>
+        </header>
+      </div>
+    </>
   );
 };
