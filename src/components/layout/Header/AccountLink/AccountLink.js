c8c4a8d3d07d5a6467c82e344d44575b04f4cef1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-16 14:31:22 +0200

    updated auth

--- src/components/layout/Header/AccountLink/AccountLink.js
+++ src/components/layout/Header/AccountLink/AccountLink.js
@@ -1,23 +1,15 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
 import { Icon } from "../../../reusable/Icon/Icon";
 
 import styles from "./AccountLink.module.scss";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { Auth } from "../../../modals/Auth/Auth";
 
-export const AccountLink = ({accountButtonClickHandler}) => {
-
-
+export const AccountLink = ({ accountButtonClickHandler }) => {
   return (
-    <>
-      <div className={styles["container"]} onClick={accountButtonClickHandler}>
-        <div>
-          <span></span>
-        </div>
-        <Icon icon={faUser} />
+    <button className={styles["container"]} onClick={accountButtonClickHandler}>
+      <div>
+        <span></span>
       </div>
-    </>
+      <Icon icon={faUser} />
+    </button>
   );
 };
