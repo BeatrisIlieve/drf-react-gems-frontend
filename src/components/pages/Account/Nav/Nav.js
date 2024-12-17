fbbb9e02bf08b8c39bcd8bae627012924b055e5f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-17 16:15:28 +0200

    updated account

--- src/components/pages/Account/Nav/Nav.js
+++ src/components/pages/Account/Nav/Nav.js
@@ -2,24 +2,26 @@ import { useState } from "react";
 import styles from "./Nav.module.scss";
 
 export const Nav = () => {
-  const [accountDetailsSelected, setAccountDetailsSelected] = useState(true);
-
-  const [orderHistorySelected, setOrderHistorySelected] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("accountDetails");
 
   return (
     <nav className={styles["nav"]}>
       <ul className={styles["nav__list"]} role="list">
         <li
           className={`${styles["nav__item"]} ${
-            accountDetailsSelected ? styles["nav__item_selected"] : ""
+            selectedItem === "accountDetails"
+              ? styles["nav__item_selected"]
+              : ""
           }`.trim()}
+          onClick={() => setSelectedItem("accountDetails")}
         >
           Account Details
         </li>
         <li
           className={`${styles["nav__item"]} ${
-            orderHistorySelected ? styles["nav__item_selected"] : ""
+            selectedItem === "orderHistory" ? styles["nav__item_selected"] : ""
           }`.trim()}
+          onClick={() => setSelectedItem("orderHistory")}
         >
           Order History
         </li>
