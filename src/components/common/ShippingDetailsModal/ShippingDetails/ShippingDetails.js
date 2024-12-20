817c11effec1ae4df0b8e8012183173ca5b46837
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-20 13:59:26 +0200

    updated user form

--- src/components/common/ShippingDetailsModal/ShippingDetails/ShippingDetails.js
+++ src/components/common/ShippingDetailsModal/ShippingDetails/ShippingDetails.js
@@ -1,14 +1,12 @@
 import styles from "./ShippingDetails.module.scss";
 
 import { ShippingDetailsForm } from "../../ShippingDetailsForm/ShippingDetailsForm";
-import { useToggleIsModalOpen } from "../../../../hooks/useToggleIsModalOpen";
-export const ShippingDetails = () => {
-  const { toggleIsModalOpen } = useToggleIsModalOpen();
 
+export const ShippingDetails = () => {
   return (
     <section className={styles["shipping-details"]}>
       <h2 className={styles["shipping-details__title"]}>Shipping Details</h2>
-      <ShippingDetailsForm toggleIsModalOpen={toggleIsModalOpen} />
+      <ShippingDetailsForm />
     </section>
   );
 };
