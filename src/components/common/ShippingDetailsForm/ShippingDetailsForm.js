f90db90b40e95f71466600551513ab066a059d2e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-20 13:59:53 +0200

    updated user form

--- src/components/common/ShippingDetailsForm/ShippingDetailsForm.js
+++ src/components/common/ShippingDetailsForm/ShippingDetailsForm.js
@@ -3,18 +3,25 @@ import { SelectFields } from "./SelectFields/SelectFields";
 
 import { Button } from "../../reusable/Button/Button";
 import { useShippingDetailsContext } from "../../../contexts/ShippingDetailsContext";
-import styles from "./ShippingDetailsForm.module.scss"
-export const ShippingDetailsForm = ({ toggleIsModalOpen }) => {
+import styles from "./ShippingDetailsForm.module.scss";
+
+import { useToggleIsModalOpen } from "../../../hooks/useToggleIsModalOpen";
+export const ShippingDetailsForm = () => {
+  const { toggleIsModalOpen } = useToggleIsModalOpen();
+
   const { submitHandler } = useShippingDetailsContext();
   return (
     <div className="container mt-5">
-    <form className={styles["shipping-details-form"]} onSubmit={(e) => submitHandler(e, toggleIsModalOpen)}>
-      <InputFields />
-      <SelectFields />
-      <div className={styles["shipping-details-form__button"]}>
-        <Button label={"Save"} color={"black"} buttonType={"submit"} />
-      </div>
-    </form>
+      <form
+        className={styles["shipping-details-form"]}
+        onSubmit={(e) => submitHandler(e, toggleIsModalOpen)}
+      >
+        <InputFields />
+        <SelectFields />
+        <div className={styles["shipping-details-form__button"]}>
+          <Button label={"Save"} color={"black"} buttonType={"submit"} />
+        </div>
+      </form>
     </div>
   );
 };
