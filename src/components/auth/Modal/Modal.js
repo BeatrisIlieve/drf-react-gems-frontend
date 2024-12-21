624426ebfa4e36e429b50e28efcaa45b1b7bbf8e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-21 14:19:11 +0200

    updated user form

--- src/components/auth/Modal/Modal.js
+++ src/components/auth/Modal/Modal.js
@@ -2,12 +2,23 @@ import { useModal } from "../../../hooks/useModal";
 import { CursorImageEffect } from "../../common/CursorImageEffect/CursorImageEffect";
 import { XMark } from "../../reusable/XMark/XMark";
 import { Form } from "../../reusable/Form/Form";
+import { InputField } from "../../reusable/InputField/InputField";
 
 export const Modal = ({ toggleIsModalOpen, isModalOpen }) => {
   const { isTransitioning, modalRef, modalCloseHandler } = useModal({
     toggleIsModalOpen,
     isModalOpen,
   });
+
+  const [contentIsTransitioning, setContentIsTransitioning] = useState(false);
+
+  const updateContentIsTransitioningHandler = () => {
+    setContentIsTransitioning(true);
+
+    setTimeout(() => {
+      setContentIsTransitioning(false);
+    }, 400);
+  };
 
   return (
     <section
@@ -27,9 +38,29 @@ export const Modal = ({ toggleIsModalOpen, isModalOpen }) => {
         }`}
       >
         <XMark callbackFunction={modalCloseHandler} />
-        <Form>
-            
-        </Form>
+        <div
+          className={`${styles["form-container"]} ${
+            contentIsTransitioning
+              ? styles["form-container_transition-out"]
+              : styles["form-container_transition-in"]
+          }`}
+        >
+          <h1 className={styles["form-container__title"]}>{formTitle}</h1>
+          <p className={styles["form-container__paragraph"]}>{formParagraph}</p>
+          <Form
+            buttonLabel={""}
+            buttonColor={""}
+            buttonType={""}
+            submitHandler={updateContentIsTransitioningHandler}
+          >
+            <InputField
+              userData={""}
+              formItems={""}
+              updateFormItems={""}
+              updateUserData={""}
+            />
+          </Form>
+        </div>
       </div>
     </section>
   );
