7cf897e5383d065068d8fd63593280c21315565f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-23 20:19:46 +0200

    updated auth

--- src/components/pages/Collection/Overview/Description/Description.js
+++ src/components/pages/Collection/Overview/Description/Description.js
@@ -4,7 +4,11 @@ export const Description = ({ title, description }) => {
   return (
     <div className={styles["description"]}>
       <h2 className={styles["description__title"]}>{title}</h2>
-      <p className={styles["description__description"]}>{description}</p>
+      {/* <p className={styles["description__description"]}>{description}</p> */}
+      <p className={styles["description__description"]}>
+        A series of fine jewelry designs that celebrate the endless <span>beauty of
+        nature’s</span> greatest gifts – rare gemstones and flowers in bloom.
+      </p>
     </div>
   );
 };
