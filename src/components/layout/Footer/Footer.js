960fe633095c15b1dc5948a2789d4dafdb37a463
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-26 12:56:18 +0200

    updated fonts

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -3,6 +3,7 @@ import "./Footer.module.scss";
 export const Footer = () => {
   return (
     <footer>
+      <p>shipping</p>
       <span>
         © 2025 <abbr title={"Django Rest Framework"}>DRF</abbr>-React Gems
       </span>
--- src/components/layout/Header/ProductSetDiscount/ProductSetDiscount.js
+++ src/components/layout/Header/ProductSetDiscount/ProductSetDiscount.js
@@ -4,7 +4,8 @@ export const ProductSetDiscount = () => {
   return (
     <div className={styles["product-set-discount"]}>
       <button className={styles["product-set-discount__button"]}>
-        ENJOY 50% DISCOUNT ON THE 6-PIECE JEWELRY SET
+        {/* ENJOY 50% DISCOUNT ON THE 6-PIECE JEWELRY SET */}
+        ENJOY FREE SHIPPING ON THE 6-PIECE JEWELRY SET
       </button>
     </div>
   );
